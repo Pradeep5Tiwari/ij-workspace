982955bf232623eb887832cbe95c7c2e5715e899
Pradeep Tiwari <pradeep.tiwari555@gmail.com>
2023-05-03 22:21:45 +0530

    About API Gateway

---
+++ Project/Spring_Security/src/main/java/com/example/spring/security/Spring_Security/controller/SpringSecurityController.java
@@ -0,0 +1,8 @@
+package com.example.spring.security.Spring_Security;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SpringSecurityController {
+
+}
---
+++ Project/Spring_Security/src/main/java/org/example/spring/security/App.java
@@ -0,0 +1,13 @@
+package org.example.spring.security;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
